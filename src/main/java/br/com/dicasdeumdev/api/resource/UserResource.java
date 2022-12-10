04e205757a3abea8c6760d4b8dcf424b05d6d565
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2022-12-10 10:35:56 -0300

    :sparkles:	feat: criação do controller

--- src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
@@ -1,18 +1,23 @@
 package br.com.dicasdeumdev.api.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.service.IUserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
     
+    @Autowired
+    IUserService userService;
+    
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "Andre", "andre@email.com", "1233"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
