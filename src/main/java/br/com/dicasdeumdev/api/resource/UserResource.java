337f2850eaaf8427b124832d1db97e3ed1ab7e62
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2022-12-17 11:59:15 -0300

    :sparkles:	feat: mapeamento para user dto

--- src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
@@ -1,12 +1,14 @@
 package br.com.dicasdeumdev.api.resource;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.dicasdeumdev.api.domain.User;
+
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.service.IUserService;
 
 @RestController
@@ -16,8 +18,12 @@ public class UserResource {
     @Autowired
     IUserService userService;
     
+    @Autowired
+    private ModelMapper mapper;
+    
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok()
+                .body(mapper.map(userService.findById(id), UserDTO.class));
     }
 }
