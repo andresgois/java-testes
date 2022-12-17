6f97bcbb7594736858069d6cad686c30437010e5
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2022-12-17 12:51:05 -0300

    :sparkles:	feat: implementação da criação de user

--- src/main/java/br/com/dicasdeumdev/api/service/IUserService.java
+++ src/main/java/br/com/dicasdeumdev/api/service/IUserService.java
@@ -3,9 +3,11 @@ package br.com.dicasdeumdev.api.service;
 import java.util.List;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 
 public interface IUserService {
     
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO user);
 }
--- src/main/java/br/com/dicasdeumdev/api/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/api/service/impl/UserServiceImpl.java
@@ -2,9 +2,12 @@ package br.com.dicasdeumdev.api.service.impl;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.repository.UserRepository;
 import br.com.dicasdeumdev.api.service.IUserService;
 import br.com.dicasdeumdev.api.service.exceptions.ObjectNotFoundException;
@@ -14,6 +17,9 @@ public class UserServiceImpl implements IUserService {
     
     @Autowired
     UserRepository userRepository;
+    
+    @Autowired
+    private ModelMapper mapper;
     
     @Override
     public User findById(Integer id) {
@@ -25,6 +31,12 @@ public class UserServiceImpl implements IUserService {
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO user) {
+        User nUser = mapper.map(user, User.class);
+        return userRepository.save(nUser);
     }
     
 }
