9d1c19ef69c4b5d57e46fae00b2b0a9a9fbd8b13
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2022-12-17 12:51:25 -0300

    :sparkles:	feat: metod de criacao de user

--- src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
@@ -39,5 +43,15 @@ public class UserResource {
                 .collect(Collectors.toList());
         return ResponseEntity.ok()
                 .body(listDto);
+    }
+    
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO user){
+        User newUser = userService.create(user);
+        URI uri = ServletUriComponentsBuilder
+                    .fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(newUser.getId())
+                    .toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
