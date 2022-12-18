4af6db9b17071a1ad6465ca2084dff1748445f06
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2022-12-18 11:10:53 -0300

    :sparkles:	feat: endpoint delete

--- src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,5 +62,11 @@ public class UserResource {
         user.setId(id);
         User updateUser = userService.update(user);
         return ResponseEntity.ok().body(mapper.map(updateUser, UserDTO.class));
+    }
+    
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
