973626829472033e5f7dd4470525769e010e4021
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-04-08 13:48:52 -0300

    :test: chore: teste do service, throw de objeto nao encontrado

--- src/test/java/br/com/dicasdeumdev/api/service/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/api/service/impl/UserServiceImplTest.java
@@ -2,13 +2,11 @@ package br.com.dicasdeumdev.api.service.impl;
 
 import java.util.Optional;
 
+import br.com.dicasdeumdev.api.service.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,6 +16,7 @@ import br.com.dicasdeumdev.api.repository.UserRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class UserServiceImplTest {
@@ -27,9 +26,9 @@ public class UserServiceImplTest {
     public static final String EMAIL = "andre@email.com";
     public static final String PASSWORD = "123";
     // Criar uma instância real
-    @InjectMocks
+    @InjectMocks    // cria uma instância real
     private UserServiceImpl service;
-    @Mock
+    @Mock           // fingi busca os dados
     private UserRepository repository;
     @Mock
     private ModelMapper mapper;
@@ -37,19 +36,32 @@ public class UserServiceImplTest {
     private UserDTO userDto;
     private Optional<User> optionalUser;
     
-    @BeforeEach
+    @BeforeEach // antes de tudo faça isso
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        MockitoAnnotations.openMocks(this); // inicia os mocks
         startUser();
     }
     
     @Test
     void whenFindByIdThenReturnAnUserInstance() {
-       Mockito.when(repository.findById(Mockito.anyInt())).thenReturn(optionalUser);
+       when(repository.findById(Mockito.anyInt())).thenReturn(optionalUser);
        User response = service.findById(ID);
        assertEquals(User.class, response.getClass());
        assertEquals(ID, response.getId());
        assertNotNull(response);
+    }
+
+    @Test
+    void whenFindByIdTheReturnAnObjectNotFoundException(){
+        when(repository.findById(ArgumentMatchers.anyInt()))
+                .thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
+
+        try {
+            service.findById(ID);
+        } catch (Exception ex){
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals("Objeto não encontrado", ex.getMessage());
+        }
     }
     
     private void startUser() {
